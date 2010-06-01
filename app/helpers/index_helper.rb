1dc70812bd36d701859159d745fac5c74f215b78
Tsuyoshi <tsuyoshi@MacBookPro.local>
2010-06-01 21:00:36 +0900

    find uri link in tweet

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -2,4 +2,9 @@ module IndexHelper
   def time_format time
     time.strftime "%Y/%m/%d %H:%M:%S %Z"
   end
+  
+  def find_uri(s)
+    uri_reg = URI.regexp(%w[http https])
+    s.gsub(uri_reg) {%Q{<a href="#{$&}" target="_blank">#{$&}</a>}}
+  end
 end
